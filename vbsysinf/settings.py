47ba8d9a388e935011105971a95743ef00159e1c
Kbyte <kbytesys@gmail.com>
2014-04-10 15:05:48 +0200

    Signed-off-by: Kbyte <kbytesys@gmail.com>

--- vbsysinf/settings.py
+++ vbsysinf/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'vbsysinf',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,7 +69,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'it-it'
 
 TIME_ZONE = 'UTC'
 
